1cd47c783cf5a7211edb7e66b7cebd094cd20f66
Emmanuel Essien <emmanuelessiens@outlook.com>
2024-07-21 14:40:46 -0600

    Update Doc

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'source/index'
+master_doc = 'index'
 
 # General information about the project.
 author  = "Emmanuel Martins"
@@ -146,7 +146,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['docs/build/html/_static']
+html_static_path = ['build/html/_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
